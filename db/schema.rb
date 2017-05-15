5c1c231e93511f44f84960f2f5a646dd8e67b928
Christos Karolemeas <karolemeaschris@gmail.com>
2017-05-15 20:12:14 +0300

    Authentication with full name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513170819) do
+ActiveRecord::Schema.define(version: 20170515163116) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170513170819) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
