c24436c38184fb4d0c69d58d130cfb90daa8adec
Christos Karolemeas <karolemeaschris@gmail.com>
2017-05-17 13:21:16 +0300

    Confirmation emails for signing up

--- Gemfile
+++ Gemfile
@@ -41,6 +41,10 @@ gem 'devise', '~> 4.2'
 
 gem 'gravatarify', '~> 3.0.0'
 
+gem 'sendgrid-rails', '~> 2.0', '>= 2.0.5'
+
+gem 'figaro'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515163116) do
+ActiveRecord::Schema.define(version: 20170517074541) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20170515163116) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "fullname"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
