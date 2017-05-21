91490f281e63f11e4e124c1a5e21a3c57ce74e24
Christos Karolemeas <karolemeaschris@gmail.com>
2017-05-21 14:31:36 +0300

    Created reservation controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,14 @@
+class ReservationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @reservation = current_user.reservations.create(reservation_params)
+
+    redirect_to @reservation.car
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :price, :total, :car_id)
+  end
+end
